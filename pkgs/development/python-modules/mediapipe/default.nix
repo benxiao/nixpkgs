5fea706b17fa859f850af366ab61a0f27586cecb
Ran Xiao <ben.xiao@me.com>
2022-10-14 09:47:13 +1100

    get mediapipe working

--- pkgs/development/python-modules/mediapipe/default.nix
+++ pkgs/development/python-modules/mediapipe/default.nix
@@ -1,30 +1,41 @@
 { lib
 , buildPythonPackage
-, fetchFromGitHub
+, fetchurl
 , python3
-, bazel_5
+, tree
+, unzip
+, zip
+, patchelf
+, stdenv
 }:
 
 buildPythonPackage rec {
   pname = "mediapipe";
-  version = "v0.8.11";
-
-  src = fetchFromGitHub {
-    owner = "google";
-    repo = pname;
-    rev = "refs/tags/${version}";
-    hash = "sha256-2lBDxTMTPU5pXcJMKA9jtFprizG9e6qB8g7HEeXZ8E8=";
+  version = "0.8.11";
+  format = "wheel";
+  src = fetchurl {
+    url = "https://files.pythonhosted.org/packages/75/b0/de86aaadb05eae01409eb1b5b8cc4b64bb0327a477b3dec8c67998755e14/mediapipe-0.8.11-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl";
+    sha256 = "4a9622deabc54ebf259f5f958789c7e92a38d01938ae0dd01de017c4eb799580";
   };
 
   doCheck = false;
 
-  
+
   preConfigure = ''
-    substituteInPlace requirements.txt \
-      --replace "opencv-contrib-python" "" 
+    ${unzip}/bin/unzip $NIX_BUILD_TOP/dist/*.whl
+
+    substituteInPlace mediapipe-0.8.11.dist-info/METADATA \
+    --replace "Requires-Dist: opencv-contrib-python" ""
+
+    ${patchelf}/bin/patchelf --replace-needed libstdc++.so.6 ${stdenv.cc.cc.lib}/lib/libstdc++.so.6 mediapipe/python/_framework_bindings.cpython-310-x86_64-linux-gnu.so
+
+    rm $NIX_BUILD_TOP/dist/mediapipe-0.8.11-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl     
+
+    ${zip}/bin/zip -r $NIX_BUILD_TOP/dist/mediapipe-0.8.11-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl \
+    mediapipe  mediapipe-0.8.11.dist-info  mediapipe.libs
   '';
 
-  nativeBuildInputs = [bazel_5];
+  nativeBuildInputs = [ ];
 
   propagatedBuildInputs = with python3.pkgs;[
     opencv4
