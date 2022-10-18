79746813bb784a15305d1f1e1e63172f85bab7fb
Ran Xiao <ben.xiao@me.com>
2022-10-18 21:13:24 +1100

    refactoring

--- pkgs/development/python-modules/mediapipe/default.nix
+++ pkgs/development/python-modules/mediapipe/default.nix
@@ -1,56 +1,53 @@
 { lib
 , buildPythonPackage
-, fetchurl
+, fetchPypi
 , python3
-, tree
-, unzip
-, zip
-, patchelf
-, stdenv
+, pythonOlder
+, pythonAtLeast
 , gtk2
+, pythonRelaxDepsHook
+, autoPatchelfHook
 }:
-let python =
+let
+  pname = "mediapipe";
+  version = "0.8.11";
+  python =
     let
-    packageOverrides = self:
-    super: {
-      opencv4 = super.opencv4.override {
-        enableGtk2 = true;
-        gtk2 = gtk2;
-        enableFfmpeg = true; #here is how to add ffmpeg and other compilation flags
+      packageOverrides = self:
+        super: {
+          opencv4 = super.opencv4.override {
+            enableGtk2 = true;
+            gtk2 = gtk2;
+            enableFfmpeg = true; #here is how to add ffmpeg and other compilation flags
+          };
         };
-    };
     in
-      python3.override {inherit packageOverrides; self = python;};
+    python3.override { inherit packageOverrides; self = python; };
 
 in
 
 buildPythonPackage rec {
-  pname = "mediapipe";
-  version = "0.8.11";
+  
+  inherit pname version;
+  disabled = pythonOlder "3.8" || pythonAtLeast "3.11";
   format = "wheel";
-  src = fetchurl {
-    url = "https://files.pythonhosted.org/packages/75/b0/de86aaadb05eae01409eb1b5b8cc4b64bb0327a477b3dec8c67998755e14/mediapipe-0.8.11-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl";
-    sha256 = "4a9622deabc54ebf259f5f958789c7e92a38d01938ae0dd01de017c4eb799580";
-  };
-
   doCheck = false;
 
+  src = let
+      pyShortVersion = "cp${builtins.replaceStrings ["."] [""] python.pythonVersion}";
+      binary-hash = (import ./binary-hashes.nix)."${pyShortVersion}";
+    in
+    fetchPypi ({
+      inherit pname version format;
+      dist = pyShortVersion;
+      python = pyShortVersion;
+      abi = pyShortVersion;
+      platform = "manylinux_2_17_x86_64.manylinux2014_x86_64";
+    } // binary-hash);
 
-  preConfigure = ''
-    ${unzip}/bin/unzip $NIX_BUILD_TOP/dist/*.whl
+  nativeBuildInputs = [ pythonRelaxDepsHook autoPatchelfHook ];
 
-    substituteInPlace mediapipe-0.8.11.dist-info/METADATA \
-    --replace "Requires-Dist: opencv-contrib-python" ""
-
-    ${patchelf}/bin/patchelf --replace-needed libstdc++.so.6 ${stdenv.cc.cc.lib}/lib/libstdc++.so.6 mediapipe/python/_framework_bindings.cpython-310-x86_64-linux-gnu.so
-
-    rm $NIX_BUILD_TOP/dist/mediapipe-0.8.11-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl     
-
-    ${zip}/bin/zip -r $NIX_BUILD_TOP/dist/mediapipe-0.8.11-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl \
-    mediapipe  mediapipe-0.8.11.dist-info  mediapipe.libs
-  '';
-
-  nativeBuildInputs = [ ];
+  pythonRemoveDeps = [ "opencv-contrib-python" "protobuf" ];
 
   propagatedBuildInputs = with python.pkgs;[
     opencv4
